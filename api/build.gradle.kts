0707daffe5ab72fa87310b67ce3e88b2f6c7ef17
Cody Engel <cengel815@gmail.com>
2022-01-09 11:39:04 -0600

    Upgrade To JDK 17, Kotlin 1.6.0-RC
    Upgraded the project to JDK 17 because why not. In the same spirit, while I was updating dependencies I thought, may as well go with Kotlin 1.6 and get a sneak preview. YOLO 🤞

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,10 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-
 plugins {
     application
-    kotlin("jvm") version "1.5.0"
-    kotlin("plugin.serialization") version "1.5.0"
+    kotlin("jvm") version "1.6.0-RC"
+    kotlin("plugin.serialization") version "1.6.0-RC"
     id("io.gitlab.arturbosch.detekt") version "1.17.1"
 }
 group = "dev.engel"
@@ -12,7 +11,6 @@ version = "0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    jcenter()
     maven {
         url = uri("https://dl.bintray.com/kotlin/ktor")
     }
@@ -20,8 +18,9 @@ repositories {
         url = uri("https://dl.bintray.com/kotlin/kotlinx")
     }
 }
+
 dependencies {
-    val ktorVersion = "1.5.3"
+    val ktorVersion = "1.6.7"
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("io.ktor:ktor-serialization:$ktorVersion")
@@ -29,22 +28,22 @@ dependencies {
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-serialization:$ktorVersion")
 
-    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("ch.qos.logback:logback-classic:1.2.10")
 
     // Google Cloud
     implementation(platform("com.google.cloud:libraries-bom:20.1.0"))
     implementation("com.google.cloud:google-cloud-datastore")
 
     // Tracing
-    val openCensusVersion = "0.28.3"
+    val openCensusVersion = "0.30.0"
     implementation("io.opencensus:opencensus-api:$openCensusVersion")
     implementation("io.opencensus:opencensus-exporter-trace-stackdriver:$openCensusVersion")
     runtimeOnly("io.opencensus:opencensus-impl:$openCensusVersion")
 
     testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
 
     testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
     testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
@@ -53,11 +52,11 @@ dependencies {
     testImplementation("io.strikt:strikt-jvm")
     testImplementation("io.strikt:strikt-mockk")
 
-    testImplementation("io.mockk:mockk:1.11.0")
+    testImplementation("io.mockk:mockk:1.12.1")
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "12"
+    kotlinOptions.jvmTarget = "17"
 }
 
 tasks.withType<Test> {
@@ -70,5 +69,5 @@ compileKotlin.kotlinOptions {
 }
 
 application {
-    mainClassName = "io.ktor.server.netty.EngineMain"
+    mainClass.set("io.ktor.server.netty.EngineMain")
 }
