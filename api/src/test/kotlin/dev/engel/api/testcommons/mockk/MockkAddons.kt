8419cfe55292b65f03fc08271483e61ca29a9331
Cody Engel <cody@engel.dev>
2026-03-02 20:51:20 -0700

    🔥🔥🔥🔥🔥
    Burn it down! Elmo on fire meme!

--- api/src/test/kotlin/dev/engel/api/testcommons/mockk/MockkAddons.kt
+++
@@ -1,8 +0,0 @@
-package dev.engel.api.testcommons.mockk
-
-import io.mockk.MockKVerificationScope
-import io.mockk.verify
-
-fun verifyOnce(verifyBlock: MockKVerificationScope.() -> Unit) {
-    verify(exactly = 1, verifyBlock = verifyBlock)
-}
